fbcf20a482901b3b89a02a3fd15235a8556b9c42
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2020-08-07 17:07:21 -0500

    more changes

--- entry.js
+++ entry.js
@@ -43,7 +43,7 @@ function serverCallback(req, res) {
         res.writeHead(statusCode, {
           'Content-type': 'application/json',
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
+          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
         });
         res.end(returnedData);
         console.log(`Responded with ${returnedData} with a statusCode of ${statusCode}`);
